8bc30da6827fc0431b45a8271bd4d7ca3e1a48ff
Abhilash <abhilasht0007.t@gmail.com>
2024-11-08 13:21:19 +0530

    middleware: implemented

--- lib/auth.ts
+++ lib/auth.ts
@@ -63,3 +63,32 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
+
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
+//   const token = request.cookies.get("token")?.value || "";
+
+//   // Define public paths
+//   const isPublicPath = path === "/signin" || path === "/signup";
+
+//   // Redirect logged-in users away from /signin or /signup to /dashboard
+//   if (isPublicPath && token) {
+//     return NextResponse.redirect(new URL("/dashboard", request.url));
+//   }
+
+//   // Redirect to /signin if trying to access protected route without a token
+//   if (!isPublicPath) {
+//     // Prevent further redirects if already on /signin
+//     if (path !== "/signin") {
+//       return NextResponse.redirect(new URL("/signin", request.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/dashboard","/admin/:path*"]
+// };
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,19 @@
+// export { auth as middleware } from "./lib/auth";
+
 import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const token = request.cookies.get("token")?.value || "";
 
-  
   // Define public paths
   const isPublicPath = path === "/signin" || path === "/signup";
+
+  // Fetch token using next-auth's getToken helper
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET
+  });
 
   // Redirect logged-in users away from /signin or /signup to /dashboard
   if (isPublicPath && token) {
@@ -15,7 +22,6 @@ export function middleware(request: NextRequest) {
 
   // Redirect to /signin if trying to access protected route without a token
   if (!isPublicPath && !token) {
-    // Prevent further redirects if already on /signin
     if (path !== "/signin") {
       return NextResponse.redirect(new URL("/signin", request.url));
     }
@@ -25,5 +31,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/dashboard", "/signin", "/signup", "/admin/:path*"]
+  matcher: ["/dashboard", "/admin/:path*"]
 };

96c73902603bca31a46136ed5574b3381da6b1c4
Abhilash <abhilasht0007.t@gmail.com>
2024-11-16 20:03:01 +0530

    .

--- app/api/freight/freightBooking/userFreightForwarderInput/route.ts
+++ app/api/freight/freightBooking/userFreightForwarderInput/route.ts
@@ -16,46 +16,48 @@ const validateInput = z.object({
   freightIsAccepted: z.boolean(),
   bookingId: z.string()
 });
- 
+
 const prisma = new PrismaClient();
 
 export async function PATCH(request: NextRequest) {
-try {
-  const userFfId = request.headers.get("x-user-id") as string;
+  try {
+    const userFfId = request.headers.get("x-user-id") as string;
 
-  const body = await request.json();
+    const body = await request.json();
 
-  const { success } = validateInput.safeParse(body);
+    const { success } = validateInput.safeParse(body);
 
-  if (!success) {
-    return NextResponse.json({
-      message: "invalid input type"
-    });
-  }
-
-  const data = await prisma.freightBooking.update({
-    where: {
-      id : body.bookingId
-    },
-    data: {
-      departureDate: body.departureDate,
-      arrivalDate: body.arrivalDate,
-      load: body.load !== undefined ? body.load : undefined,
-      noOfContainers: body.noOfContainers !== undefined ? body.noOfContainers : undefined,
-      price: body.price,
-      containerType: body.containerType !== undefined ? body.containerType : undefined,
-      freightIsAccepted: body.freightIsAccepted,
-      freightForwarderId : userFfId
+    if (!success) {
+      return NextResponse.json({
+        message: "invalid input type"
+      });
     }
-  })
 
-  return NextResponse.json({
-    data: data,
-    status: 200,
-  })
-} catch (err) {
-  return NextResponse.json({
-    error : err
-  })
+    const data = await prisma.freightBooking.update({
+      where: {
+        id: body.bookingId
+      },
+      data: {
+        departureDate: body.departureDate,
+        arrivalDate: body.arrivalDate,
+        load: body.load !== undefined ? body.load : undefined,
+        noOfContainers:
+          body.noOfContainers !== undefined ? body.noOfContainers : undefined,
+        price: body.price,
+        containerType:
+          body.containerType !== undefined ? body.containerType : undefined,
+        freightIsAccepted: body.freightIsAccepted,
+        freightForwarderId: userFfId
+      }
+    });
+
+    return NextResponse.json({
+      data: data,
+      status: 200
+    });
+  } catch (err) {
+    return NextResponse.json({
+      error: err
+    });
   }
 }

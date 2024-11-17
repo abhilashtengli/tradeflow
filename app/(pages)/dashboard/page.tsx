9f8251cec269b713228c72d34c1c6eb13a8aec47
Abhilash <abhilasht0007.t@gmail.com>
2024-11-17 21:05:10 +0530

    NavBar

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import NavBar from "@/components/NavBar";
 import React from "react";
 
 const page = () => {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      <NavBar />
+      dashboard
+    </div>
+  );
 };
 
 export default page;
---
+++ components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div className="w-full border border-black py-4">NavBar</div>;
+};
+
+export default NavBar;
